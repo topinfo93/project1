5a6b0cc89204b657f71a501b4dc572aef78c4347
Quang Tran <topinfo93@gmail.com>
2017-12-16 21:18:30 +0700

    disable admin bar and don't allow use admin dashboard ( only user )

--- wp-content/themes/happyship/functions.php
+++ wp-content/themes/happyship/functions.php
@@ -106,3 +106,21 @@ function myEndSession() {
     session_destroy ();
 }
 
+
+add_action('after_setup_theme', 'remove_admin_bar');
+ 
+function remove_admin_bar() { 
+    if (!current_user_can('administrator') && !is_admin()) {
+      show_admin_bar(false);
+    }
+}
+
+add_action( 'init', 'blockusers_init' );
+
+function blockusers_init() {
+    if ( is_admin() && ! current_user_can( 'administrator' ) &&
+    ! ( defined( 'DOING_AJAX' ) && DOING_AJAX ) ) {
+    wp_redirect( home_url() );
+    exit;
+    }
+}
